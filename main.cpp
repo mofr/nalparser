a391db15102adafb7fc91a8f4773bc7f50340df7
mofr <mofr@zond.org>
2015-10-10 15:41:09 +0600

    mafaka

--- main.cpp
+++ main.cpp
@@ -2,148 +2,86 @@
 #include "Arguments.h"
 #include "BlockingQueue.h"
 
-//const char* nal_types[] = {"TRAIL_N",
-//"TRAIL_R",
-//"TSA_N",
-//"TSA_R",
-//"STSA_N",
-//"STSA_R",
-//"RADL_N",
-//"RADL_R",
-//"RASL_N",
-//"RASL_R",
-//"RSV_VCL_N10",
-//"RSV_VCL_N12",
-//"RSV_VCL_N14",
-//"RSV_VCL_R11",
-//"RSV_VCL_R13",
-//"RSV_VCL_R15",
-//"BLA_W_LP",
-//"BLA_W_RADL",
-//"BLA_N_LP",
-//"IDR_W_RADL",
-//"IDR_N_LP",
-//"CRA_NUT",
-//"RSV_IRAP_VCL22",
-//"RSV_IRAP_VCL23",
-//"RSV_VCL24..",
-//"RSV_VCL31",
-//"VPS_NUT",
-//"SPS_NUT",
-//"PPS_NUT",
-//"AUD_NUT",
-//"EOS_NUT",
-//"EOB_NUT",
-//"FD_NUT"
-//"PREFIX_SEI_NUT"
-//"SUFFIX_SEI_NUT"
-//"RSV_NVCL41"
-//"RSV_NVCL47"
-//};
-//
-//struct nal_unit_header
-//{
-//    int forbidden_zero_bit : 1;
-//    unsigned int nal_unit_type : 6;
-//    unsigned int nuh_layer_id : 6;
-//    unsigned int nuh_temporal_id_plus1 : 3;
-//};
-//
-//int parse_nal(int argc, char ** argv)
-//{
-//    int nalCount = 0;
-//    static const int BUFFER_SIZE = 1024*8;
-//    char buf[BUFFER_SIZE];
-//    int read;
-//    int filesize = 0;
-//    int continuous = 0;
-//    while(!feof(input))
-//    {
-//        read = fread(buf, 1, BUFFER_SIZE, input);
-//        if(ferror(input))
-//        {
-//            std::cout << "File read error" << std::endl;
-//            break;
-//        }
-//        filesize += read;
-//        for(int i = 0; i < read; ++i)
-//        {
-//            if((continuous == 0 && buf[i] == 0x00) ||
-//               (continuous == 1 && buf[i] == 0x00) ||
-//               (continuous == 2 && buf[i] == 0x00) ||
-//               (continuous == 3 && buf[i] == 0x01))
-//            {
-//                continuous += 1;
-//            }
-//            else
-//            {
-//                if(continuous == 4)
-//                {
-//                    nalCount += 1;
-//
-//                    if(i + 2 >= read)
-//                    {
-//                        std::cout << "buffer over" << std::endl;
-//                    }
-//                    else
-//                    {
-//                        nal_unit_header * h = (nal_unit_header*)(buf + i);
-//                        std::cout << h->nal_unit_type << std::endl;
-//                    }
-//                }
-//                continuous = 0;
-//            }
-//        }
-//    }
-//
-//    std::cout << "File size = " << filesize << std::endl;
-//    std::cout << "NAL unit count = " << nalCount << std::endl;
-//
-//    fclose(input);
-//    return 0;
-//}
+const char* nalTypes[] = {
+        "TRAIL_N",
+        "TRAIL_R",
+        "TSA_N",
+        "TSA_R",
+        "STSA_N",
+        "STSA_R",
+        "RADL_N",
+        "RADL_R",
+        "RASL_N",
+        "RASL_R",
+        "RSV_VCL_N10",
+        "RSV_VCL_N12",
+        "RSV_VCL_N14",
+        "RSV_VCL_R11",
+        "RSV_VCL_R13",
+        "RSV_VCL_R15",
+        "BLA_W_LP",
+        "BLA_W_RADL",
+        "BLA_N_LP",
+        "IDR_W_RADL",
+        "IDR_N_LP",
+        "CRA_NUT",
+        "RSV_IRAP_VCL22",
+        "RSV_IRAP_VCL23",
+        "RSV_VCL24",
+        "RSV_VCL25",
+        "RSV_VCL26",
+        "RSV_VCL28",
+        "RSV_VCL29",
+        "RSV_VCL30",
+        "RSV_VCL31",
+        "VPS_NUT",
+        "SPS_NUT",
+        "PPS_NUT",
+        "AUD_NUT",
+        "EOS_NUT",
+        "EOB_NUT",
+        "FD_NUT",
+        "PREFIX_SEI_NUT",
+        "SUFFIX_SEI_NUT",
+        "RSV_NVCL41",
+        "RSV_NVCL42",
+        "RSV_NVCL43",
+        "RSV_NVCL44",
+        "RSV_NVCL45",
+        "RSV_NVCL46",
+        "RSV_NVCL47"
+};
 
+const char * nalTypeAsString(int type)
+{
+    if(type < 0 || type > 47)
+    {
+        return "UNKNOWN";
+    }
+    else
+    {
+        return nalTypes[type];
+    }
+}
 
-//void variant1()
-//{
-//    NalReader reader(filename);
-//
-//    struct NalUnit
-//    {
-//        int type;
-//        int elapsedMillis;
-//    };
-//
-//    auto inputFunction = [&reader](BinaryNalUnit & binaryNalUnit) {
-//        return reader.readNext(binaryNalUnit);
-//    };
-//
-//    auto processFunction = [](const BinaryNalUnit & binaryNalUnit, NalUnit & nalUnit) {
-//        nalUnit.type = binaryNalUnit.fakeData * 2;
-//        nalUnit.elapsedMillis = 400 + (binaryNalUnit.fakeData % 2) * 400;
-//        std::this_thread::sleep_for(std::chrono::milliseconds(nalUnit.elapsedMillis));
-//    };
-//
-//    auto outputFunction = [](const NalUnit & nalUnit) {
-//        std::cout << nalUnit.type << " " << nalUnit.elapsedMillis << " ms" << std::endl;
-//    };
-//
-//    ProcessingQueue<BinaryNalUnit, NalUnit> processingQueue;
-//    processingQueue.start(threadCount, inputFunction, processFunction, outputFunction);
-//    processingQueue.waitForFinished();
-//}
+struct NalUnit
+{
+    long offset;
+    int type;
+
+    NalUnit(long offset, int type) : offset(offset), type(type)
+    { }
+};
 
 struct Chunk
 {
     long offset = 0;
     long size = 0;
-    char * data = nullptr;
-    Chunk * next = nullptr;
-
+    unsigned char * data = nullptr;
 
     Chunk(long size) : size(size)
     {
-        data = new char[size];
+        data = new unsigned char[size];
     }
 
     ~Chunk()
@@ -151,21 +89,69 @@ struct Chunk
         delete[] data;
     }
 
+    void setNext(std::shared_ptr<Chunk> next)
+    {
+        std::unique_lock<std::mutex> lock(nextMutex);
+        this->next = next;
+        this->nextInitialized = true;
+        nextCondition.notify_all();
+    }
+
+    std::shared_ptr<Chunk> getNext() const
+    {
+        std::unique_lock<std::mutex> lock(nextMutex);
+        nextCondition.wait(lock, [this](){return nextInitialized;});
+        return next;
+    }
+
     const std::vector<long> & getStartCodePrefixes()
     {
         std::unique_lock<std::mutex> lock(mutex);
         if(!parsed)
         {
-            // @todo find startCodePrefixPositions in data
+            findStartCodePrefixes();
+            parsed = true;
         }
 
-        return startCodePrefixPositions;
+        return startCodePrefixes;
     }
 
 private:
-    std::mutex mutex;// @todo read-write lock
+    // @todo parse chunk + chunk->next
+    void findStartCodePrefixes()
+    {
+        int matchCount = 0;
+        for(long i = 0; i < size; ++i)
+        {
+            if((matchCount == 0 && data[i] == 0x00) ||
+               (matchCount == 1 && data[i] == 0x00) ||
+               (matchCount == 2 && data[i] == 0x00) ||
+               (matchCount == 3 && data[i] == 0x01))
+            {
+                ++matchCount;
+            }
+            else
+            {
+                if(matchCount == 4)
+                {
+                    startCodePrefixes.push_back(i);
+//                    int nalType = (data[i] & 0x01111110) >> 1;
+//                    nalUnits.emplace_back(offset + i - 3, nalType);
+                }
+                matchCount = 0;
+            }
+        }
+    }
+
+private:
+    std::mutex mutex;
     bool parsed = false;
-    std::vector<long> startCodePrefixPositions;
+    std::vector<long> startCodePrefixes;
+
+    mutable std::mutex nextMutex;
+    mutable std::condition_variable nextCondition;
+    bool nextInitialized = false;
+    std::shared_ptr<Chunk> next;
 };
 
 class ChunkReader
@@ -188,13 +174,13 @@ public:
         return file != nullptr;
     }
 
-    Chunk * readNext()
+    std::shared_ptr<Chunk> readNext()
     {
         if(feof(file))
         {
             if(previous)
             {
-                Chunk * chunk = previous;
+                std::shared_ptr<Chunk> chunk = previous;
                 previous = nullptr;
                 return chunk;
             }
@@ -209,17 +195,17 @@ public:
             previous = readChunk();
         }
 
-        Chunk * chunk = previous;
-        chunk->next = readChunk();
-        previous = chunk->next;
+        std::shared_ptr<Chunk> chunk = previous;
+        chunk->setNext(readChunk());
+        previous = chunk->getNext();
         return chunk;
     }
 
 private:
-    Chunk * readChunk()
+    std::shared_ptr<Chunk> readChunk()
     {
-        // @todo chunk pool
-        Chunk * chunk = new Chunk(ChunkSize);
+        // @todo chunk pool (threadsafe)
+        std::shared_ptr<Chunk> chunk(new Chunk(ChunkSize));
         chunk->size = fread(chunk->data, 1, ChunkSize, file);
         chunk->offset = offset;
         offset += chunk->size;
@@ -229,22 +215,38 @@ private:
 private:
     FILE * file = nullptr;
     long offset = 0;
-    Chunk * previous = nullptr;
+    std::shared_ptr<Chunk> previous;
 
     static const int ChunkSize = 1024*1024;
 };
 
-void chunkParser(BlockingQueue<Chunk*> & chunkQueue)
+class Collector
 {
-    Chunk * chunk;
+public:
+    void collect(NalUnit nalUnit)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+    }
+
+private:
+    std::mutex mutex;
+};
+
+void chunkParser(BlockingQueue<std::shared_ptr<Chunk>> & chunkQueue)
+{
+    long nalUnitCount = 0;
+    std::shared_ptr<Chunk> chunk;
     while(chunkQueue.pop(chunk))
     {
-        std::cout << chunk->offset << " " << chunk->next << std::endl;
-        // @todo parse chunk + chunk->next
+        for(auto & startCodePrefixPos : chunk->getStartCodePrefixes())
+        {
+            std::cout << startCodePrefixPos << std::endl;
+            ++nalUnitCount;
+        }
         // @todo output to collector
-
-        delete chunk;
     }
+
+    std::cout << "Nal unit count: " << nalUnitCount << std::endl;
 }
 
 /**
@@ -262,17 +264,24 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    BlockingQueue<Chunk*> chunkQueue(args.threadCount * 2);
+    BlockingQueue<std::shared_ptr<Chunk>> chunkQueue(args.threadCount * 2);
 
-    std::thread thread(chunkParser, std::ref(chunkQueue));
+    std::vector<std::thread> threads;
+    for(int i = 0; i < args.threadCount; ++i)
+    {
+        threads.emplace_back(chunkParser, std::ref(chunkQueue));
+    }
 
-    while(Chunk * chunk = reader.readNext())
+    while(std::shared_ptr<Chunk> chunk = reader.readNext())
     {
         chunkQueue.push(chunk);
     }
     chunkQueue.close();
 
-    thread.join();
+    for(auto & thread : threads)
+    {
+        thread.join();
+    }
 
     return 0;
 }
